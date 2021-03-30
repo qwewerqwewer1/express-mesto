c59506ada39489f9f7e3f7f8fde74e30c9af7ca5
Dima Kovyazin <kovyazinaa11@yandex.ru>
2021-03-30 07:32:53 +0300

    userRouter

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-const router = require('express').Router;
+const userRouter = require('express').Router;
 const { createUser } = require('../controllers/users');
 
-router.post('/user', createUser); // mancraft   p.s. the game))
+userRouter.post('/user', createUser); // mancraft   p.s. the game))
 
-module.exports = { router };
+module.exports = userRouter;
