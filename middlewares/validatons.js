89e9f0d05a7f7f188258034cc1e768821c826ac0
Dima Kovyazin <kovyazinaa11@yandex.ru>
2021-04-26 08:10:19 +0300

    changed after reviews

--- middlewares/validatons.js
+++ middlewares/validatons.js
@@ -73,7 +73,9 @@ const validateGetCards = celebrate({
 const validatePostCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: validator.isUrl({ require_protocol: true })
+    link: Joi.string().custom((value) => {
+      validator.isUrl(value, { require_protocol: true })
+    })
   }),
   headers: Joi.object().keys({
     'content-type': Joi.string().valid('application/json').required(),
